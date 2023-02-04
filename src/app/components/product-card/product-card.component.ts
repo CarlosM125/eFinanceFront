de9f76f8b38f24297fe6c27110f9273e7e343fb4
Juan Avilés <jaaviles@puce.edu.ec.com>
2023-02-03 22:45:49 -0500

    Cambios Juan

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { SimpleOuterSubscriber } from 'rxjs/internal/innerSubscribe';
+import { AccountsService } from 'src/app/services/accounts.service';
 
 @Component({
   selector: 'app-product-card',
@@ -10,8 +13,33 @@ export class ProductCardComponent implements OnInit {
   @Input() amount: string;
   @Input() color: string;
 
-  constructor() { }
+  constructor(public alertController: AlertController, private accountService: AccountsService) {}
 
   ngOnInit() {}
 
+  async alertDelete(){
+    const alert = await this.alertController.create({
+      header:'Eliminar cuenta',
+      message:'¿Seguro quieres eliminar esta cuenta?',
+      buttons:[{
+        text:'Si',
+        role:'yes',
+        handler:() =>{
+          this.delete();
+        },
+        },
+        {
+          text:'No',
+          role:'no',
+          handler:() =>{
+            console.log('cancelado');
+          },
+        },
+        ]
+    });
+    await alert.present();
+  }
+
+  delete(){
+  }
 }
